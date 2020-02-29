8fc183942dadd56ae06561acc5c148265d315492
Daniel Meechan <daniel.meechan.1+git@gmail.com>
2020-02-29 23:33:18 +0000

    Add game loop

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,19 @@ import './App.css';
 
 import Map from './components/Map';
 import Hud from './components/Hud';
+import { useInterval } from './hooks/useInterval';
+
+const TICK_INTERVAL = 100; // 100 milliseconds
+
+function tick() {
+  // Update the game state
+}
 
 function App() {
+  useInterval(() => {
+    tick();
+  }, TICK_INTERVAL)
+
   return (
     <div className="App">
       Big Foil
---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,27 @@
+import { useRef, useEffect } from "react";
+
+export function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval
+  useEffect(() => {
+    function tick() {
+      if (savedCallback && savedCallback.current) {
+        // @ts-ignore
+        savedCallback.current();
+      }
+    }
+
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => {
+        clearInterval(id);
+      };
+    }
+  }, [delay]);
+}
